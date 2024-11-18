50bae5825f8364f4c319b70e8dd87e0fb6dbe64c
Ishwar Patel <ishwarrpatel50@gmail.com>
2024-11-18 23:40:07 +0530

    add two num sum

--- add_two_number.py
+++ add_two_number.py
@@ -1,6 +1,23 @@
 
 
-def addnums(a,b):
-    return a + b
+# def addnums(a,b):
+#     return a + b
 
-print(addnums(5,4))
+# print(addnums(5,4))
+
+# def addnums(a,b):
+#     num1 = int(input("Enter first Number"))
+#     num2 = int(input("Enter Second Number"))
+#     a = num1
+#     b = num2
+#     return a + b
+
+
+
+
+def add(n, m):
+  return n+m
+num1 = int(input("Enter 1st number:"))
+num2 = int(input("Enter 2nd number:"))
+result = add(num1, num2)
+print("Addition of num1 and num2 are: {}".format(result))
