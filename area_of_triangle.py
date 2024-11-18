a1aa12526b881ffb6572f2364d8df6060d2ac251
Ishwar Patel <ishwarrpatel50@gmail.com>
2024-11-19 02:18:21 +0530

    add py programs

---
+++ area_of_triangle.py
@@ -0,0 +1,33 @@
+
+
+
+# first thing accept i dont know what is area of triangle
+# search
+# done
+# i have to find how much area has taken by triangle
+# mistake i rushed and didnt take time to understand what is meaning of area of triangles
+# create function triangle_area
+# inside have to calulate base and height and then divide with 2
+# formula for calculate area of triangles is (base * height) / 2
+# this operation i have to perform with declare variable calculate_area
+# return result
+
+
+def triangle_area(base,height):
+    calculate_area = (base * height) / 2
+    return calculate_area
+base = 22
+height = 22
+
+result =  triangle_area(base,height)
+print(result)
+
+
+
+
+
+
+
+
+
+
---
+++ swap_variables.py
@@ -0,0 +1,12 @@
+# Swap two variables
+
+
+a = 10
+b = 20
+
+print(f"before swapping a has {a} and b has {b}")
+# c = a
+# a = b
+# b = c
+a ,b = b, a
+print(f"after swapping a has {a} and b has {b}")
